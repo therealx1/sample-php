9aefac9c7b3266429a68ba20ba6d642342cd766b
solanki ravi maheshbhai <123919864+therealx1@users.noreply.github.com>
2023-09-07 01:06:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,31 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+$databaseHost = 'stu-do-user-14623249-0.b.db.ondigitalocean.com';
+	$databaseName = 'defaultdb';
+	$databaseUsername = 'parth';
+	$databasePassword = 'AVNS_c16XxROSTRMnvh_Ihto';
 
-use Cowsayphp\Farm;
+	$con = mysqli_connect($databaseHost,$databaseUsername,$databasePassword,$databaseName);
 
-header('Content-Type: text/plain');
-
-$text = "Set a message by adding ?message=<message here> to the URL";
-if(isset($_GET['message']) && $_GET['message'] != '') {
-	$text = $_GET['message'];
+if (isset($_POST['btn'])) {
+	if ($con) {
+echo "Success";
+	}
+	else{
+		echo "Not Connected";
+	}
 }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Welcome</title>
+</head>
+<body>
+<form method="post">
+	<button type="submit" name="btn" >Check</button>
+</form>
+</body>
+</html>
 
-$cow = Farm::create(\Cowsayphp\Farm\Cow::class);
-echo $cow->say($text);
